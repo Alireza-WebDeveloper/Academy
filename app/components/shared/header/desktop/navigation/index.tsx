cf317ebf57951261ca9d0507ebf2570568f76af3
unknown <alireza_webdev>
2024-08-15 11:44:39 +0330

    update link component

--- app/components/shared/header/desktop/navigation/index.tsx
+++ app/components/shared/header/desktop/navigation/index.tsx
@@ -3,7 +3,9 @@ import DropDownItem from '../../../ui/link-dropdown';
 const Navigation = () => {
   return (
     <div className="flex items-center gap-14 ml-auto  z-10">
-      <LinkContainer className="text-white">خانه</LinkContainer>
+      <LinkContainer href={'/'} className="text-white">
+        خانه
+      </LinkContainer>
       <DropDownItem />
       <LinkContainer className="text-white">لورم ایپسوم</LinkContainer>
       <LinkContainer className="text-white">لورم ایپسوم</LinkContainer>
--- app/components/shared/header/logo/index.tsx
+++ app/components/shared/header/logo/index.tsx
@@ -1,13 +1,16 @@
 import { LogoHeaderIcon } from '../../ui/icons';
+import LinkContainer from '../../ui/link-container';
 
 const Logo = () => {
   return (
-    <section className="ml-auto flex items-center gap-2">
-      <LogoHeaderIcon />
-      <h1 className="text-white text-xl leading-none font-bold tracking-tight">
-        تینگز زیاد
-      </h1>
-    </section>
+    <LinkContainer href={'/'}>
+      <section className="ml-auto flex items-center gap-2">
+        <LogoHeaderIcon />
+        <h1 className="text-white text-xl leading-none font-bold tracking-tight">
+          تینگز زیاد
+        </h1>
+      </section>
+    </LinkContainer>
   );
 };
 
